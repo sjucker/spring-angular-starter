c94c416c7d230224cddf13e8730002c44312acc6
Stefan Jucker <jucker@mptechnology.ch>
2022-10-25 22:01:29 +0200

    Fix error handling

--- src/main/webapp/app/interceptors/authentication-interceptor.service.ts
+++ src/main/webapp/app/interceptors/authentication-interceptor.service.ts
@@ -12,23 +12,30 @@ export class AuthenticationInterceptor implements HttpInterceptor {
     }
 
     intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-        const token = this.authenticationService.getAuthorizationToken();
-        if (token) {
-            return next.handle(request.clone({
-                setHeaders: {Authorization: 'Bearer ' + token}
-            }));
-        }
+        return next.handle(this.addAuthToken(request)).pipe(
+            catchError((error: HttpErrorResponse) => this.handleAuthError(error))
+        );
+    }
 
-        return next.handle(request).pipe(catchError(error => this.handleAuthError(error)));
+    addAuthToken(request: HttpRequest<any>): HttpRequest<any> {
+        const token = this.authenticationService.getAuthorizationToken();
+        if (!token) {
+            return request;
+        }
+        return request.clone({
+            setHeaders: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
     }
 
     private handleAuthError(err: HttpErrorResponse): Observable<any> {
         if (err.status === 401 || err.status === 403) {
+            this.authenticationService.logout();
             this.router.navigate([LOGIN_PATH]);
             return of(err.message);
         }
         return throwError(() => err);
     }
-
 
 }
